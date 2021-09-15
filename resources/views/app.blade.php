84cef7b2b8a4f35cdb3a47205444f5ab8f58fe62
Felipe Alves Reis <contacto.felipereis@gmail.com>
2021-09-16 00:10:45 +0100

    fix footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,110 +30,29 @@
 @stop
 
 @section('footer')
-<footer class="main-footer" style="display: flex; justify-content: center; align-items: center;">
+<div class="main-footer" style="display: flex; justify-content: space-between; align-items: center;">
     <strong>Made with love!</strong>
     
     <p style="display: flex; justify-content: center; align-items: center;">
         <span>Donate:</span>
         
         <label class="btc donate-crypto-box">
-            <div class="coin">
-                <div class="coin-face">
-                    <svg height="10" viewBox="0 0 32 32" width="10" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21.78 15.37c.51-.61.83-1.4.83-2.26 0-2.74-1.6-4.38-4.24-4.38V5.45c0-.12-.1-.22-.22-.22h-1.27c-.11 0-.2.1-.2.21v3.3h-1.7V5.44c0-.12-.1-.22-.22-.22H13.5c-.12 0-.2.1-.21.21v3.3H9.67c-.12 0-.21.09-.21.21v1.31c0 .12.1.22.21.22h.21c.94 0 1.7.79 1.7 1.75v7c0 .92-.68 1.67-1.55 1.75a.21.21 0 0 0-.18.16l-.33 1.32c-.01.06 0 .13.04.19.04.05.1.08.17.08h3.55v3.3c0 .1.1.2.2.2h1.28c.12 0 .21-.1.21-.22v-3.28h1.7v3.3c0 .1.1.2.21.2h1.27c.12 0 .22-.1.22-.22v-3.28h.85c2.65 0 4.24-1.64 4.24-4.37 0-1.28-.68-2.39-1.68-3zm-6.8-4.01h2.54c.94 0 1.7.78 1.7 1.75 0 .96-.76 1.75-1.7 1.75h-2.55v-3.5zm3.39 8.75h-3.4v-3.5h3.4c.93 0 1.7.78 1.7 1.75 0 .96-.77 1.75-1.7 1.75z" fill="#FFF"></path>
-                    </svg>
-                </div>
-                <div class="coin-face"></div>
-                <div class="coin-face"></div>
-                <div class="coin-face"></div>
-                <div class="coin-face">
-                    <svg height="10" viewBox="0 0 32 32" width="10" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21.78 15.37c.51-.61.83-1.4.83-2.26 0-2.74-1.6-4.38-4.24-4.38V5.45c0-.12-.1-.22-.22-.22h-1.27c-.11 0-.2.1-.2.21v3.3h-1.7V5.44c0-.12-.1-.22-.22-.22H13.5c-.12 0-.2.1-.21.21v3.3H9.67c-.12 0-.21.09-.21.21v1.31c0 .12.1.22.21.22h.21c.94 0 1.7.79 1.7 1.75v7c0 .92-.68 1.67-1.55 1.75a.21.21 0 0 0-.18.16l-.33 1.32c-.01.06 0 .13.04.19.04.05.1.08.17.08h3.55v3.3c0 .1.1.2.2.2h1.28c.12 0 .21-.1.21-.22v-3.28h1.7v3.3c0 .1.1.2.21.2h1.27c.12 0 .22-.1.22-.22v-3.28h.85c2.65 0 4.24-1.64 4.24-4.37 0-1.28-.68-2.39-1.68-3zm-6.8-4.01h2.54c.94 0 1.7.78 1.7 1.75 0 .96-.76 1.75-1.7 1.75h-2.55v-3.5zm3.39 8.75h-3.4v-3.5h3.4c.93 0 1.7.78 1.7 1.75 0 .96-.77 1.75-1.7 1.75z" fill="#FFF"></path>
-                    </svg>
-                </div>
-            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="100%" height="100%" version="1.1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"
+                viewBox="0 0 4091.27 4091.73"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:xodm="http://www.corel.com/coreldraw/odm/2003">
+                <g id="Layer_x0020_1">
+                <metadata id="CorelCorpID_0Corel-Layer"/>
+                <g id="_1421344023328">
+                <path fill="#F7931A" fill-rule="nonzero" d="M4030.06 2540.77c-273.24,1096.01 -1383.32,1763.02 -2479.46,1489.71 -1095.68,-273.24 -1762.69,-1383.39 -1489.33,-2479.31 273.12,-1096.13 1383.2,-1763.19 2479,-1489.95 1096.06,273.24 1763.03,1383.51 1489.76,2479.57l0.02 -0.02z"/>
+                <path fill="white" fill-rule="nonzero" d="M2947.77 1754.38c40.72,-272.26 -166.56,-418.61 -450,-516.24l91.95 -368.8 -224.5 -55.94 -89.51 359.09c-59.02,-14.72 -119.63,-28.59 -179.87,-42.34l90.16 -361.46 -224.36 -55.94 -92 368.68c-48.84,-11.12 -96.81,-22.11 -143.35,-33.69l0.26 -1.16 -309.59 -77.31 -59.72 239.78c0,0 166.56,38.18 163.05,40.53 90.91,22.69 107.35,82.87 104.62,130.57l-104.74 420.15c6.26,1.59 14.38,3.89 23.34,7.49 -7.49,-1.86 -15.46,-3.89 -23.73,-5.87l-146.81 588.57c-11.11,27.62 -39.31,69.07 -102.87,53.33 2.25,3.26 -163.17,-40.72 -163.17,-40.72l-111.46 256.98 292.15 72.83c54.35,13.63 107.61,27.89 160.06,41.3l-92.9 373.03 224.24 55.94 92 -369.07c61.26,16.63 120.71,31.97 178.91,46.43l-91.69 367.33 224.51 55.94 92.89 -372.33c382.82,72.45 670.67,43.24 791.83,-303.02 97.63,-278.78 -4.86,-439.58 -206.26,-544.44 146.69,-33.83 257.18,-130.31 286.64,-329.61l-0.07 -0.05zm-512.93 719.26c-69.38,278.78 -538.76,128.08 -690.94,90.29l123.28 -494.2c152.17,37.99 640.17,113.17 567.67,403.91zm69.43 -723.3c-63.29,253.58 -453.96,124.75 -580.69,93.16l111.77 -448.21c126.73,31.59 534.85,90.55 468.94,355.05l-0.02 0z"/>
+                </g>
+                </g>
+            </svg>
             <input class="coin-address" onclick="this.select()" readonly="readonly" spellcheck="false" type="text" value="bc1q9jms8369pzl5frll77pzscqzya5exy866wf40r" />
         </label>
     </p>
-</footer>
-@stop
-
-@section('css')
-    <style>
-        .donate-crypto-box {
-            display: flex;
-            align-items: center;
-            width: 100%;
-            padding: 1em;
-            box-sizing: border-box;
-            user-select: none;
-            cursor: text;
-        }
-
-        .coin {
-            display: inline-block;
-            position: relative;
-            min-width: 2em;
-            min-height: 2em;
-            animation: spin 3s cubic-bezier(.3,2,.4,.8) infinite both;
-            transform-style: preserve-3d;
-            vertical-align: middle;
-            
-            @keyframes spin {
-                0%, 10% {
-                    transform: rotate(-10deg) perspective(400px);
-                }
-                90%, 100% {
-                    transform: rotate(-10deg) perspective(400px) rotateY(180deg);
-                }
-            }
-        }
-
-        .coin-face {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            border-radius: 50%;
-            
-            &:nth-child(1) { transform: translateZ(-.2em) rotateY(-180deg); }
-            &:nth-child(2) { transform: translateZ(-.1em); }
-            &:nth-child(4) { transform: translateZ(.1em); }
-            &:nth-child(5) { transform: translateZ(.2em); }
-            
-            svg {
-                width: 100%;
-                height: 100%;
-            }
-
-            background: shade(#ff9900, 35%);
-            
-            &:nth-child(1),
-            &:nth-child(5) {
-                background: #ff9900;
-            }
-        }
-
-        .coin-address {
-            flex: 1;
-            font: .7em/2.5 Monaco, monospace;
-            text-align: center;
-            margin-left: 1em;
-            border-width: 0 0 2px;
-            border-color: rgba(black, .1);
-            transition: border-color .3s;
-            cursor: text;
-
-            &:hover { transition-duration: .1s; }
-
-            &:hover, &:focus { border-color: #ff9900; }
-        }
-
-        .btc {
-            max-width: 21em;
-        }
-    </style>
+</div>
 @stop
 
 @section('js')
